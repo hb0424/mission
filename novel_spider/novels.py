5809b30382ec5831e110fada3fd07148b95a45cb
houbin <bin.hou@risksyun.com>
2018-01-11 09:26:35 +0800

    增加小说爬虫

---
+++ novel_spider/novels.py
@@ -0,0 +1,83 @@
+gNoversConfig = {
+    "websites":{
+        "www.biqudao.com":{
+            "hostname": "https://www.biqudao.com",
+            "webname": "笔趣岛",
+            "book_name": "<h1>([\u4e00-\u9fa5]+)</h1>",
+            "book_author": "<p>作.*者：(.*)</p>",
+            "book_summary": "<div id=\"intro\">\\r\\n\s*(.*)",
+            "book_chapter": "<dd> <a style(.*)</dd>",
+            "book_chapter_name": ">(.*)</a>",
+            "book_chapter_url": "href=\"(.*)\">",
+            "book_chapter_content": "<div id=\"content\">([.\s\S]*)<script>chaptererror"
+        },
+        "www.biquge.com.tw":{
+            "hostname": "http://www.biquge.com.tw",
+            "webname": "笔趣阁",
+            "book_name": "<h1>([\u4e00-\u9fa5]+)</h1>",
+            "book_author": "<p>作.*者：(.*)</p>",
+            "book_summary": "<div id=\"intro\">\\r\\n\s*(.*)",
+            "book_chapter": "<dd><a (.*?)</dd>",
+            "book_chapter_name": ">(.*)</a>",
+            "book_chapter_url": "href=\"(.*)\">",
+            "book_chapter_content": "<div id=\"content\">([.\s\S]*)<script>read3"
+        },
+        "www.biqugezw.com":{
+            "hostname": "http://www.biqugezw.com",
+            "webname": "笔趣阁中文网",
+            "book_name": "<h1>([\u4e00-\u9fa5]+)</h1>",
+            "book_author": "<p>作.*者：(.*)</p>",
+            "book_summary": "<div id=\"intro\">\\r\\n\s*<p>(.*)</p>",
+            "book_chapter": "<dd><a (.*)</dd>",
+            "book_chapter_name": ">(.*)</a>",
+            "book_chapter_url": "href=\"(.*)\">",
+            "book_chapter_content": "<div id=\"content\">([.\s\S]*)<script>read3"
+        },
+        "www.zwdu.com":{
+            "hostname": "https://www.zwdu.com",
+            "webname": "八一中文网",
+            "book_name": "<h1>([\u4e00-\u9fa5]+)</h1>",
+            "book_author": "<p>作.*者：(.*)</p>",
+            "book_summary": "<div id=\"intro\">[.\s]*?<p>(.*)</p>",
+            "book_chapter": "<dd><a (.*?)</dd>",
+            "book_chapter_name": ">(.*)</a>",
+            "book_chapter_url": "href=\"(.*)\">",
+            "book_chapter_content": "<div id=\"content\">([.\s\S]*)<script>read3"
+        }
+
+        
+    },
+    "books":[
+        {
+            "urls":[
+                "https://www.biqudao.com/bqge19874/"
+            ]
+        },
+        {
+            "urls":[
+                "https://www.zwdu.com/book/15127/"
+            ]
+        },
+        {
+            "urls":[
+                "http://www.biquge.com.tw/14_14969/"
+            ]
+        },
+        {
+            "urls":[
+                "http://www.biqugezw.com/23_23496/"
+            ]
+        },
+        {
+            "urls":[
+                "https://www.biqudao.com/bqge8146/"
+            ]
+        },
+        {
+            "urls":[
+                "https://www.biqudao.com/bqge110911/"
+            ]
+        }
+    ]
+}
+
---
+++ novel_spider/nover_spider.py
@@ -0,0 +1,180 @@
+import re
+import urllib.request
+import urllib.parse
+import json
+import sys
+import os
+import threading
+import datetime
+import time
+import ctypes
+from novels import gNoversConfig
+import sqlite3
+from string import Template
+
+
+def GetCurFileDir():
+    path = sys.path[0]
+    if os.path.isdir(path):
+        return path
+    elif os.path.isfile(path):
+        return os.path.dirname(path)
+
+def DecodeBytes(byteHtml):
+    decodeTypes = ["utf-8", "gbk", "gb2312", "gb18030", "hz", "utf_16"]
+    for decodeType in decodeTypes:
+        try:
+            strHtml = byteHtml.decode(decodeType)
+        except UnicodeDecodeError as e:
+            continue
+        except UnicodeError as e:
+            continue
+        else:
+            return strHtml
+    
+
+def download(url, num_retries=3):
+    print('Downloading[', num_retries, ']:',  url)
+    html = None
+    try:
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+        req = urllib.request.Request(url=url, headers=headers)
+        html = urllib.request.urlopen(req).read()
+    except urllib.error.HTTPError as e:
+        print('Download HTTPError: ' + e.reason)
+        html = None
+        if num_retries > 0:
+            return download(url, num_retries - 1)
+    except urllib.error.URLError as e:
+        print("download URLError: " + str(e.args))
+        html = None
+        if num_retries > 0:
+            return download(url, num_retries - 1)
+    except TimeoutError as e:
+        print("download timeout: " + url)
+        html = None
+        if num_retries > 0:
+            return download(url, num_retries - 1)
+    except Exception as e:
+        print("download Exception: " + url)
+        html = None
+        if num_retries > 0:
+            return download(url, num_retries - 1)
+
+    return html
+
+
+def parseHtml(url, webRoul):
+    strHtml = DecodeBytes(download(url))
+    bookNames = re.findall(webRoul["book_name"], strHtml)
+    if len(bookNames) <= 0:
+        print("无法解析书名")
+        return
+    bookName = bookNames[0].strip(" \r\n\\")
+    print("开始爬取：" + bookName)
+
+    bookAuthors = re.findall(webRoul["book_author"], strHtml)
+    bookAuthor = ""
+    if len(bookAuthors) > 0:
+        bookAuthor = bookAuthors[0].strip(" \r\n\\")
+
+    bookSummarys = re.findall(webRoul["book_summary"], strHtml)
+    bookSummary = ""
+    if len(bookSummarys) > 0:
+        bookSummary = bookSummarys[0].strip(" \r\n\\")
+
+    bookChapters = re.findall(webRoul["book_chapter"], strHtml)
+    if len(bookChapters) <= 0:
+        print("无法解析书目录" + bookName)
+        return
+
+    ################
+    # books:
+    # book_id book_name book_author book_summary
+    # chapters:
+    # chapter_id book_id book_source book_chapter_name book_chapter_url book_chapter_content
+    # ############
+    databasePath = GetCurFileDir()
+    databasePath += '\\novels.db'
+    conn = sqlite3.connect(databasePath)
+    bookId = 0
+    try:
+        cursor = conn.cursor()
+        # 取book_id,没有book_id则增加新书
+        results = cursor.execute(
+            "select book_id, book_name, book_author, book_summary from books WHERE book_name=?", [bookName])
+        books = results.fetchall()
+        if len(books) <= 0:
+            results = cursor.execute("INSERT INTO books (book_id, book_name, book_author, book_summary) \
+                VALUES (NULL, ?, ?, ?)",
+                                     [bookName, bookAuthor, bookSummary])
+            books = results.fetchall()
+            bookId = results.lastrowid
+        else:
+            bookId = books[0][0]
+    except (sqlite3.Error,) as e:
+        print('sqlite3 error: ' + str(e.args))
+        conn.commit()
+        conn.close()
+        return
+
+    for bookChapter in bookChapters:
+        bookChapterNames = re.findall(
+            webRoul["book_chapter_name"], bookChapter)
+        if len(bookChapterNames) <= 0:
+            print("无法解析章节名：" + bookChapter + " " +
+                  webRoul["book_chapter_name"])
+            continue
+        bookChapterName = bookChapterNames[0].strip(" \r\n\\")
+
+        bookChapterUrls = re.findall(webRoul["book_chapter_url"], bookChapter)
+        if len(bookChapterUrls) <= 0:
+            print("无法解析章节地址：" + bookChapter +
+                  " " + webRoul["book_chapter_url"])
+            continue
+        bookChapterUrl = webRoul["hostname"] + bookChapterUrls[0]
+        # 取chapter_id,没有chapter_id则增加新章节
+        try:
+            results = cursor.execute("select chapter_id, book_id, book_source, book_chapter_name from chapters \
+            WHERE book_id=? and book_source=? and book_chapter_name=?",
+                                     [bookId, webRoul["webname"], bookChapterName])
+            chapters = results.fetchall()
+            if len(chapters) <= 0:
+                contentHtml = download(bookChapterUrl)
+                if contentHtml == None:
+                    print("下载章节内容为空：" + bookChapterUrl)
+                    continue
+                strContentHtml = DecodeBytes(contentHtml)
+
+                bookChapterContents = re.findall(
+                    webRoul["book_chapter_content"], strContentHtml)
+                if len(bookChapterContents) <= 0:
+                    print("无法解析章节内容：" + bookChapterName + " " +
+                          bookChapterUrl + " " + webRoul["book_chapter_content"])
+                    continue
+                bookChapterContent = bookChapterContents[0].strip(" \r\n\\")
+
+                results = cursor.execute("INSERT INTO chapters (chapter_id, book_id, book_source, book_chapter_name, book_chapter_url, book_chapter_content) \
+                    VALUES (NULL, ?, ?, ?, ?, ?)", [bookId, webRoul["webname"], bookChapterName, bookChapterUrl, bookChapterContent])
+                print("新增章节：" + bookName + ":" + bookChapterName)
+        except (sqlite3.Error,) as e:
+            print('sqlite3 error: ' + str(e.args))
+            continue
+    conn.commit()
+    conn.close()
+    print("爬取完成：" + bookName)
+
+
+def main():
+    webSites = gNoversConfig["websites"]
+    books = gNoversConfig['books']
+    for book in books:
+        urls = book["urls"]
+        for url in urls:
+            urlObj = urllib.parse.urlparse(url)
+            webRoul = webSites[urlObj.hostname]
+            parseHtml(url, webRoul)
+
+
+if __name__ == '__main__':
+    main()
